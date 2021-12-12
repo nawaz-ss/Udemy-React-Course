bd04d468a8f1710b2c7fcc9e38d52e6ebfe94b04
nawaz_ss <sharief.shaik@virtoustack.com>
2021-12-12 13:52:45 +0530

    Composition concepts and Working with components

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,12 +1,13 @@
+import Card from "../UI/Card"
 import ExpenseItem from "./ExpenseItem"
 import './Expenses.css'
 
 function Expenses(props) {
     const { expenses } = props
     return (
-        <div className="expenses">
+        <Card className="expenses">
             {expenses.map(el => <ExpenseItem data={el} />)}
-        </div>
+        </Card>
     )
 }
 
